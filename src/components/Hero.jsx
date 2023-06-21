7731345760e9cb009a22cd85683d25a3f00a59e1
Khalil Rezgui <khalilrezgui0@gmail.com>
2023-06-21 03:12:47 +0100

    fix: typo in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Khalil</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I am student harnessing cloud technologies, <br className='sm:block hidden' />
+          I am a student harnessing cloud technologies, <br className='sm:block hidden' />
           And a full stack developer
           </p>
         </div>
