40ee0011f2fc027d66fdfbea5b2ec55ae7c2f203
Khalil Rezgui <khalilrezgui0@gmail.com>
2023-06-21 03:48:00 +0100

    Fix: click to scroll button more visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
+import ReactSound from "react-sound";
+import LoFi from "./assets/lofi.mp3"
+import { useState } from "react";
 const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
+    <ReactSound url={LoFi}
+    loop={true}
+    playStatus={isPlaying ? ReactSound.status.PLAYING : ReactSound.status.PAUSED}>
+    </ReactSound>
+      <div onClick={handlePlay} className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
